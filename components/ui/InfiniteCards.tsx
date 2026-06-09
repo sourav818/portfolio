f192ea4cbabf53b586d6ea3cbb9e07d0c42f103e
Sourav Paul <souravpaul043@gmail.com>
2026-06-09 23:21:17 +0530

    Removed testimonial profile images

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -115,7 +115,7 @@ export const InfiniteMovingCards = ({
               </span>
               <div className="relative z-20 mt-6">
   <span className="flex flex-col gap-1">
-                  {/* change text color, font-normal to font-bold, text-xl */}
+                  
                   <span className="text-xl font-bold leading-[1.6] text-white">
                     {item.name}
                   </span>
