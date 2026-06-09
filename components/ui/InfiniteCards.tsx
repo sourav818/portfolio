cb20514347ae0b6f36d5755b6ceb9d4b6844af0b
Sourav Paul <souravpaul043@gmail.com>
2026-06-09 23:18:13 +0530

    update infinity card

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -113,12 +113,8 @@ export const InfiniteMovingCards = ({
               <span className=" relative z-20 text-sm md:text-lg leading-[1.6] text-white font-normal">
                 {item.quote}
               </span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
-                {/* add this div for the profile img */}
-                <div className="me-3">
-                  <img src="/profile.svg" alt="profile" />
-                </div>
-                <span className="flex flex-col gap-1">
+              <div className="relative z-20 mt-6">
+  <span className="flex flex-col gap-1">
                   {/* change text color, font-normal to font-bold, text-xl */}
                   <span className="text-xl font-bold leading-[1.6] text-white">
                     {item.name}
